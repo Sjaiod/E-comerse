4849546b332f1013d6dc26865243fd2b9c6fd245
Sjaiod <delwar.saj2008@gmail.com>
2024-04-14 19:14:48 +0600

    a

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,20 +2,30 @@
 import Nav from '@/components/Nav';
 import React, { useEffect, useState, Suspense } from 'react';
 import { useParams,useSearchParams } from 'next/navigation';
-import { Card, CardBody, CardFooter, Image, useDisclosure } from "@nextui-org/react";
+import { Button, Card, CardBody, CardFooter, Image, Input, Link, useDisclosure } from "@nextui-org/react";
 import ProductsModal from '@/components/ProductsModal';
 
 const Page = () => {
-    const [list, setList] = useState([]);
-    const [activeModal, setActiveModal] = useState(null);
-    const { search } = useParams(); // Destructure search directly from useParams
-    const searchh = useSearchParams().get("search"); // useSearchParams can be used with useSuspense() for data fetching
+    const [emai,setEmail]=useState()
+    const [password,setPassword]=useState()
 
     const getData = async () => {
         try {
-            const getdata = await fetch(`/api/v2/products/search?search=${searchh}`);
-            const data = await getdata.json();
-            setList(data);
+            const res = await fetch('/api/v1/user', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: emai,
+                    password: password
+                })
+            });
+            const ref=await res.json()
+            if(ref){
+                localStorage.setItem("user",ref.user)
+            }
+            
         } catch (error) {
             console.log(error);
         }
@@ -23,52 +33,30 @@ const Page = () => {
 
     useEffect(() => {
         getData();
-    }, [search]);
+    }, []);
 
     return (
-        <main className='min-h-screen'>
-            <Nav />
-            <section className='flex flex-col items-center justify-center px-10 py-10'>
-                <h2 className='text-4xl capitalize text-zinc-100'>{search}</h2>
-                <div className="gap-2 grid grid-cols-2 sm:grid-cols-5">
-                    {list && list.map((item, index) => (
-                        <Card key={index} onPress={() => setActiveModal(item._id)} shadow="sm" isPressable>
-                            <CardBody className="overflow-visible p-0">
-                                <Image
-                                    shadow="sm"
-                                    radius="lg"
-                                    width="100%"
-                                    alt={item.title}
-                                    className="w-full object-cover h-[140px]"
-                                    src={item.image}
-                                />
-                            </CardBody>
-                            <CardFooter className="text-small justify-between">
-                                <b className="truncate max-w-[10rem] overflow-hidden overflow-ellipsis">{item.title}</b>
-                                <p className="text-default-500">{item.price}</p>
-                            </CardFooter>
-                            <ProductsModal
-                                title={item.title}
-                                price={item.price}
-                                description={item.description}
-                                id={item._id}
-                                image={item.image}
-                                isOpen={activeModal === item._id}
-                                onClose={() => setActiveModal(null)}
-                            />
-                        </Card>
-                    ))}
-                </div>
+        <div className='flex container items-center justify-center h-screen'>
+        <div className=' w-[790px] h-[590px] bg-cyan-600 rounded-[100%]  absolute    blur-[100px] '>
 
-            </section>
-        </main>
+        </div>
+
+<form  className='px-5 py-4  border flex items-center z-10 justify-center flex-col border-zinc-600 rounded-md shadow-lg bg-gray-900' action="POST">
+    <h2>Login</h2>
+    <div className='flex gap-4 items-center justify-center flex-col'>
+    <Input onChange={(e:any)=>setEmail(e.target.value)} type="email" variant="bordered" label="Email" className="w-[20rem]" />
+<Input type="text" onChange={(e:any)=>setPassword(e.target.value)}  variant="bordered" label="Password" />
+<p className='text-sm'>Dont have an account
+<Link className='text-blue-600' href="/auth/login">Register</Link>
+</p>
+<Button variant='solid' className='bg-cyan-600 w-[20rem]' type='submit'>Submit</Button>
+
+    </div>
+    </form>
+    </div>
     );
 };
 
-const SuspendedPage = () => (
-    <Suspense fallback={<div>Loading...</div>}>
-        <Page />
-    </Suspense>
-);
 
-export default SuspendedPage;
+
+export default Page;
